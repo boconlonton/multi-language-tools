403bfa2ca0320547fd8771c5b49db1c9633bcf62
bocon <tan.truong@go2joy.vn>
2021-05-20 17:02:39 +0700

    Added models Project,Screen

--- project/models.py
+++ project/models.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Vocabulary(models.Model):
-    """Vocabulary model"""
-    vocab_key = models.CharField(max_length=255, unique=True)
-    english_definition = models.TextField(blank=True, null=True, default="")
-    vn_definition = models.TextField(blank=True, null=True, default="")
-    korean_definition = models.TextField(blank=True, null=True, default="")
+class Project(models.Model):
+    """Project model"""
+    project_name = models.CharField(max_length=255, unique=True)
+    project_description = models.TextField()
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_modified')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created')
     modified_at = models.DateTimeField(auto_now=True)
--- screen/models.py
+++ screen/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Vocabulary(models.Model):
-    """Vocabulary model"""
-    vocab_key = models.CharField(max_length=255, unique=True)
-    english_definition = models.TextField(blank=True, null=True, default="")
-    vn_definition = models.TextField(blank=True, null=True, default="")
-    korean_definition = models.TextField(blank=True, null=True, default="")
+class Screen(models.Model):
+    """Screen model"""
+    screen_code = models.CharField(max_length=255, unique=True)
+    screen_name = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.TextField()
+    project = models.ForeignKey(to='Project', on_delete=models.CASCADE)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_modified')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created')
     modified_at = models.DateTimeField(auto_now=True)
