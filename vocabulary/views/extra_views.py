14413bf6c03c0a05bcd49b70d7f867e34f70a98d
bocon <tan.truong@go2joy.vn>
2021-05-31 14:29:54 +0700

    Update page number export

--- vocabulary/views/extra_views.py
+++ vocabulary/views/extra_views.py
@@ -39,9 +39,8 @@ def vocab_export_home(request):
         vocabs = Vocabulary.objects.filter(
             screen__id=screen.id
         ).order_by('id')
-        paginator = Paginator(vocabs, 1)
+        paginator = Paginator(vocabs, 10)
         page_number = request.GET.get('page')
-        print(page_number)
         page_obj = paginator.get_page(page_number)
     return render(
         request,
@@ -82,7 +81,7 @@ def vocab_export_home_with_code(request, screen_code):
         vocabs = Vocabulary.objects.filter(
             screen__id=screen.id
         ).order_by('id')
-        paginator = Paginator(vocabs, 1)
+        paginator = Paginator(vocabs, 10)
         page_number = request.GET.get('page') or 1
         page_obj = paginator.get_page(page_number)
     return render(
